5e9fe344d715c9ed3641dc115b00608daab5cf1b
Tarek Moubarak <tarek@hellotree.co>
2017-10-17 21:05:05 +0300

    Final Code Commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var titleButton: UIButton!
+    @IBOutlet weak var welcomeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func welcomePressed(_ sender: Any) {
+        background.isHidden = false;
+        titleButton.isHidden = false;
+        welcomeButton.isHidden = true;
+    }
+    
+    @IBAction func titlePressed(_ sender: Any) {
+        background.isHidden = true;
+        titleButton.isHidden = true;
+        welcomeButton.isHidden = false;
+    }
 }
 
